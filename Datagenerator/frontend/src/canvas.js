6196bdd41eb70b36f8205128a22c514d1fcb8a4c
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2023-11-25 18:53:47 -0600

    Added compatability for mobiles

--- Datagenerator/frontend/src/canvas.js
+++ Datagenerator/frontend/src/canvas.js
@@ -1,6 +1,4 @@
 import React, { useRef, useEffect, useState } from 'react';
-// const labels=["dog","cat","bird","fish","horse","cow","sheep"];
-
 
 const Canvas = () => {
   const canvasRef = useRef(null);
@@ -14,68 +12,110 @@ const Canvas = () => {
     ctx.lineCap = 'round';
     ctx.lineWidth = 5;
 
-    const startDrawing = (e) => {
-      setIsDrawing(true);
-      setPrevPos({
-        x: e.clientX - canvas.getBoundingClientRect().left,
-        y: e.clientY - canvas.getBoundingClientRect().top,
-      });
+    const getMousePos = (e) => {
+      const rect = canvas.getBoundingClientRect();
+      return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+      };
     };
 
-    const draw = (e) => {
+    const getTouchPos = (e) => {
+      const rect = canvas.getBoundingClientRect();
+      const touch = e.touches[0];
+      const scaleX = canvas.width / rect.width;
+      const scaleY = canvas.height / rect.height;
+    
+      return {
+        x: (touch.clientX - rect.left) * scaleX,
+        y: (touch.clientY - rect.top) * scaleY,
+      };
+    };
+    
+
+    const startDrawing = (pos) => {
+      setIsDrawing(true);
+      setPrevPos(pos);
+    };
+
+    const draw = (pos) => {
       if (!isDrawing) return;
+
       ctx.beginPath();
       ctx.moveTo(prevPos.x, prevPos.y);
-      ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+      ctx.lineTo(pos.x, pos.y);
       ctx.stroke();
-      setPrevPos({
-        x: e.clientX - canvas.getBoundingClientRect().left,
-        y: e.clientY - canvas.getBoundingClientRect().top,
-      });
+
+      setPrevPos(pos);
     };
 
     const endDrawing = () => {
       setIsDrawing(false);
     };
 
-    canvas.addEventListener('mousedown', startDrawing);
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mouseup', endDrawing);
-    canvas.addEventListener('mouseout', endDrawing);
+    const handleStart = (e) => {
+      e.preventDefault();
+      const pos = e.type === 'mousedown' ? getMousePos(e) : getTouchPos(e);
+      startDrawing(pos);
+    };
+
+    const handleMove = (e) => {
+      e.preventDefault();
+      const pos = e.type === 'mousemove' ? getMousePos(e) : getTouchPos(e);
+      draw(pos);
+    };
+
+    const handleEnd = () => {
+      endDrawing();
+    };
+
+    canvas.addEventListener('mousedown', handleStart);
+    canvas.addEventListener('mousemove', handleMove);
+    canvas.addEventListener('mouseup', handleEnd);
+    canvas.addEventListener('mouseout', handleEnd);
+
+    canvas.addEventListener('touchstart', handleStart, { passive: false });
+    canvas.addEventListener('touchmove', handleMove, { passive: false });
+    canvas.addEventListener('touchend', handleEnd);
+    canvas.addEventListener('touchcancel', handleEnd);
 
     return () => {
-      canvas.removeEventListener('mousedown', startDrawing);
-      canvas.removeEventListener('mousemove', draw);
-      canvas.removeEventListener('mouseup', endDrawing);
-      canvas.removeEventListener('mouseout', endDrawing);
+      canvas.removeEventListener('mousedown', handleStart);
+      canvas.removeEventListener('mousemove', handleMove);
+      canvas.removeEventListener('mouseup', handleEnd);
+      canvas.removeEventListener('mouseout', handleEnd);
+
+      canvas.removeEventListener('touchstart', handleStart);
+      canvas.removeEventListener('touchmove', handleMove);
+      canvas.removeEventListener('touchend', handleEnd);
+      canvas.removeEventListener('touchcancel', handleEnd);
     };
   }, [isDrawing, prevPos]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#fff'; // Set the background color to white
-    ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill the canvas with white color
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }, []);
 
   const clearCanvas = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#fff'; // Set the background color to white
-    ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill the canvas with white color
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   };
 
   return (
     <div>
-    <canvas
-      ref={canvasRef}
-      width={800}
-      height={600}
-      style={{ border: '1px solid #000' }}
-    />
-    <button onClick={clearCanvas}>Clear</button>
-    <button id="generate">Generate</button>
-
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={600}
+        style={{ border: '1px solid #000', touchAction: 'none' }}
+      />
+      <button onClick={clearCanvas}>Clear</button>
+      <button id="generate">Generate</button>
     </div>
   );
 };
